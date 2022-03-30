9c8732564dd5321814aa60aad00b3ddaffe5597e
gusflopes <gusflopes86@gmail.com>
2022-03-30 15:34:50 -0400

    persist on sqlite3

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module main
+module esquenta
 
 go 1.18
+
+require github.com/mattn/go-sqlite3 v1.14.12
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
 	x := "Gustavo Lopes"
@@ -11,5 +16,22 @@ func main() {
 }
 
 func listCourses(w http.ResponseWriter, r *http.Request) {
+	persistCourse()
 	w.Write([]byte("Hello World"))
+}
+
+func persistCourse() error {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		return err
+	}
+	stmt, err := db.Prepare("insert into courses values($1, $2)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec("1", "Curso Full Cycle")
+	if err != nil {
+		return err
+	}
+	return nil
 }
